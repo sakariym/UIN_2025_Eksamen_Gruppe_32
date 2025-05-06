db50a03719ade4d13dea02b3e6874e9d0d3a4562
Sakariya <sakariym@hiof.no>
2025-05-06 13:38:39 +0200

    Routing between Home and Event

---
+++ Billetlyst/src/components/EventPage.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router";
+
+function EventPage (){
+    return (
+       <div>
+ <h1>Event</h1>
+ <Link to="/">Home</Link>
+ </div>   
+    
+    );
+ }
+ 
+ export default EventPage;
+ 
+ 
---
+++ Billetlyst/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router";
+function Home (){
+   return (
+      <div>
+<h1>Billettlyst</h1>
+
+<h2><Link to="/EventPage">Event</Link>
+</h2>
+</div>
+   );
+}
+
+export default Home;
+
