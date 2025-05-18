3a21ba852ffad0979ada86f86ea266fd67ad308a
Abdulkalik <abdi16310@gmail.com>
2025-05-18 20:19:35 +0200

    oppdatert

--- Billetlyst/src/App.jsx
+++ Billetlyst/src/App.jsx
@@ -4,19 +4,16 @@ import Home from './components/Home'
 import EventPage from './components/EventPage'
 import Dashboard from './components/Dashboard'
 
-
 function App() {
-  
-return(
-  <>
-    <Routes>
-    <Route path="/" element={<Home />}></Route>
-    <Route path="/EventPage" element={<EventPage />} />
-    <Route path="/Dashboard" element={<Dashboard />} />
-    </Routes>
-
-  </>
-)
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/event/:id" element={<EventPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
