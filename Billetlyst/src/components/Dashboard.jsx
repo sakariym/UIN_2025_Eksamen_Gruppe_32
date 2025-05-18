47f0fb26d8b76bb548ce14c5bc67bacf53c32ab0
Sakariya <sakariym@hiof.no>
2025-05-18 16:06:15 +0200

    Created a nav and dashboard component

---
+++ Billetlyst/src/components/Dashboard.jsx
@@ -0,0 +1,26 @@
+
+function Dashboard (){
+    return (
+       <div>
+ <h1>Dashboard</h1>
+ <h2>Logg Inn</h2>
+ <form>
+    Email:
+    <label>
+        <input type="text" name="email" />
+    </label>
+    <br></br>
+    <br></br>
+    Passord:
+    <label>
+        <input type="text" name="passord" />
+    </label>
+ </form>
+ </div>   
+    
+    );
+ }
+ 
+ export default Dashboard;
+ 
+ 
