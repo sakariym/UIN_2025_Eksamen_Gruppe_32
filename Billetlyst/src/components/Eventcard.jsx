732d5bcf70458be85d8f3036c9d4802fee759c1b
Sakariya <sakariym@hiof.no>
2025-05-19 11:46:13 +0200

    Event card is now clickable

--- Billetlyst/src/components/Eventcard.jsx
+++ Billetlyst/src/components/Eventcard.jsx
@@ -1,24 +1,24 @@
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-export default function EventCard ({event}) {
+export default function EventCard({ event }) {
   return (
-< article className ="event-card">
-  {/* Viser event-bilde fra API */}
-<img
-src={event.images?.[0]?.url}
-alt={event.name}
-style={{ width: "100%", borderRadius:"8px"}}
-/>
-{/* Viser kategori (genre) */}
-<p>{event.classifications?.[0]?.genre?.name}</p>
-{/* Viser event-navn */}
-<h3>{event.name}</h3>
-{/* Viser dato */}
-<p>{event.dates?.start?.localDate}</p>
-{/* Klikkbar lenke til EventPage */}
-<Link to={`/event/${event.id}`}>Se detaljer</Link>
-
-
-</article>
+    <Link to={`/event/${event.id}`}>
+      Se detaljer
+      <article className="event-card">
+        {/* Viser event-bilde fra API */}
+        <img
+          src={event.images?.[0]?.url}
+          alt={event.name}
+          style={{ width: "100%", borderRadius: "8px" }}
+        />
+        {/* Viser kategori (genre) */}
+        <p>{event.classifications?.[0]?.genre?.name}</p>
+        {/* Viser event-navn */}
+        <h3>{event.name}</h3>
+        {/* Viser dato */}
+        <p>{event.dates?.start?.localDate}</p>
+        {/* Klikkbar lenke til EventPage */}
+      </article>
+    </Link>
   );
 }
